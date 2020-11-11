2759f5cf12bc7c160d62ece8195fc425448af9f1
Jthomas <jacob_thomas@intuit.com>
2020-11-10 17:06:00 -0800

    Adding basic generic components

---
+++ voter-app/src/hooks/useForm.ts
@@ -0,0 +1,25 @@
+import {useState, ChangeEvent} from 'react';
+
+export type HTMLFormControls = 
+| HTMLInputElement
+| HTMLSelectElement
+| HTMLTextAreaElement;    
+
+export type UseForm = <T>(initialForm: T) => [ T, (e: ChangeEvent<HTMLFormControls>) => void, () => void];
+
+export const useForm: UseForm = (initialForm) => {
+    const [ form, setForm ] = useState(initialForm); 
+
+    const change = (e: ChangeEvent<HTMLFormControls>) => {
+    
+        setForm({
+            ...form, //object spread operation, copies props of colorform to new object
+            [e.target.name]: e.target.type === "number" ? Number(e.target.value) : e.target.value,
+        });
+    
+    };
+
+    const resetForm = () => setForm(initialForm);
+
+    return [ form, change, resetForm];
+};
