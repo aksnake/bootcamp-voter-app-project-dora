2b9f21257f48155b54b9ee6b392cd96eb845879a
sbharathkumar <sendhil_grandhibharathkumar@intuit.com>
2020-11-12 12:28:14 -0800

    Refactor Voter Reducer

--- voter-app/src/reducers/voterAppReducers.ts
+++ voter-app/src/reducers/voterAppReducers.ts
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 import {electionsReducer} from "../elections/reducers/electionReducers";
-
+import {voterReducer} from "../voter/reducers/voterReducers";
 
 export const voterAppReducer = combineReducers({
+    voter: voterReducer,
     elections: electionsReducer,
 });
---
+++ voter-app/src/voter/reducers/voterReducers.tsx
@@ -0,0 +1,64 @@
+import { Reducer, combineReducers } from "redux";
+
+import { VotersSort } from "../../models/voterApp";
+import { Voter } from "../models/voters";
+import {
+  VoterActions,
+  isEditVoterAction,
+  isCancelVoterAction,
+  isSortVotersAction,
+  isRefreshVotersDoneAction,
+} from "../actions/voterActions";
+
+export const votersReducer: Reducer<Voter[], VoterActions> = (voters = [], action) => {
+  if (isRefreshVotersDoneAction(action)) {
+    return action.payload.voters;
+  }
+
+  return voters;
+};
+
+export const editVoterIdReducer: Reducer<number, VoterActions> = (
+  editVoterId = -1,
+  action
+) => {
+  if (isEditVoterAction(action)) {
+    return action.payload.voterId;
+  }
+
+  if (isCancelVoterAction(action) || isRefreshVotersDoneAction(action)) {
+    return -1;
+  }
+
+  return editVoterId;
+};
+
+export const votersSortReducer: Reducer<VotersSort, VoterActions> = (
+  votersSort = { sortCol: "id", sortDir: "asc" },
+  action
+) => {
+  if (isSortVotersAction(action)) {
+    if (
+      votersSort.sortCol === action.payload.sortCol &&
+      votersSort.sortDir === "asc"
+    ) {
+      return {
+        sortCol: action.payload.sortCol,
+        sortDir: "desc",
+      };
+    } else {
+      return {
+        sortCol: action.payload.sortCol,
+        sortDir: "asc",
+      };
+    }
+  }
+
+  return votersSort;
+};
+
+export const voterReducer = combineReducers({
+  voters: votersReducer,
+  editVoterId: editVoterIdReducer,
+  votersSort: votersSortReducer,
+});
